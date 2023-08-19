5704fbc9797961ca6c6e3c6ab2b36c8025c7a791
francis15-0 <nnadiekwemakuo.11@gmail.com>
2023-08-19 17:19:51 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let userRating = 0; 
+const button = document.querySelectorAll(".btn");
+for(let i = 0; i <= button.length; i++){
+    button[i].addEventListener("click", function(){
+        this.classList.toggle("clicked");
+    userRating = this.innerHTML
+    console.log(userRating);
+    });
+}
+
+function test(e){
+    console.log("hello");
+    document.querySelector("#one").classList.add("hidden");
+    document.querySelector("#two").classList.remove("hidden");
+    document.querySelector(".rating").innerHTML = "You selected " + userRating + " out of 5";
+}
